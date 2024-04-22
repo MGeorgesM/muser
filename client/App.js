8c06e84dd0e7815d3eda6b33056e23fb3b21037c
George <g.m.6@live.com>
2024-04-22 12:13:50 +0300

    client auth in progress

--- client/App.js
+++ client/App.js
@@ -5,13 +5,15 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Chat from './screens/Chat';
 import SignIn from './screens/SignIn';
+import SignUp from './screens/SignUp';
 
 const Stack = createStackNavigator();
 
 const ChatStack = () => {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Chat" component={SignIn} options={{headerShown:false}}/>
+            <Stack.Screen name="SignIn" component={SignIn} options={{headerShown:false}}/>
+            <Stack.Screen name="SignUp" component={SignUp} options={{headerShown:false}}/>
         </Stack.Navigator>
     );
 };
--- client/screens/SignUp.jsx
+++ client/screens/SignUp.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../config/firebase';
 import {
     StyleSheet,
@@ -14,8 +13,10 @@ import {
 } from 'react-native';
 
 const logoImg = require('../assets/logo.png');
+const { styles } = require('./SignIn');
 
-const SignIn = ({ navigation }) => {
+const SignUp = () => {
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
@@ -35,13 +36,20 @@ const SignIn = ({ navigation }) => {
                     <Image style={styles.welcomeLogo} source={logoImg} />
                     <Text style={styles.header}>Welcome Back!</Text>
                     <View>
+                        <Text style={styles.label}>Name</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Georges Mouawad"
+                            value={name}
+                            onChangeText={(text) => setName(text)}
+                        />
                         <Text style={styles.label}>Email</Text>
                         <TextInput
                             style={styles.input}
                             placeholder="user@muser.com"
                             value={email}
                             keyboardType="email-address"
-                            textContentType='emailAddress'
+                            textContentType="emailAddress"
                             onChangeText={(text) => setEmail(text)}
                         />
                         <Text style={styles.label}>Password</Text>
@@ -60,9 +68,9 @@ const SignIn = ({ navigation }) => {
                         <Text style={styles.primaryBtnText}>Log In</Text>
                     </TouchableOpacity>
                     <Text style={styles.promptText}>
-                        Don't have an account?{' '}
+                        Have an account?{' '}
                         <Text style={styles.promptLink} onPress={() => navigation.navigate('SignUp')}>
-                            Register
+                            Login
                         </Text>
                     </Text>
                 </View>
@@ -71,73 +79,4 @@ const SignIn = ({ navigation }) => {
     );
 };
 
-export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    scrollContainer: {
-        flexGrow: 1,
-        justifyContent: 'space-between',
-        paddingHorizontal: 20,
-    },
-    header: {
-        textAlign: 'center',
-        fontSize: 32,
-        fontWeight: 'bold',
-        marginTop: 24,
-        marginBottom: 44,
-    },
-    label: {
-        fontSize: 16,
-        marginBottom: 6,
-        marginLeft: 8,
-    },
-    input: {
-        height: 48,
-        borderRadius: 24,
-        borderWidth: 0.5,
-        borderColor: '#212529',
-        padding: 16,
-        marginBottom: 14,
-    },
-    primaryBtn: {
-        height: 48,
-        borderRadius: 24,
-        backgroundColor: '#212529',
-        borderRadius: 24,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    primaryBtnText: {
-        color: '#fff',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-
-    promptText: {
-        fontSize: 16,
-        marginTop: 10,
-        textAlign: 'center',
-    },
-
-    promptLink: {
-        color: 'blue',
-        textDecorationLine: 'underline',
-    },
-
-    welcomeLogo: {
-        width: 130,
-        height: 130,
-        alignSelf: 'center',
-    },
-
-    topInnerContainer: {
-        marginTop: 128,
-    },
-
-    bottomInnerContainer: {
-        marginBottom: 64,
-    },
-});
-
-export default SignIn;
+export default SignUp;
