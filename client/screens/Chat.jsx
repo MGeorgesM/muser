fb896d9742917a042e551fece09b1db295fda112
George <g.m.6@live.com>
2024-04-23 13:34:56 +0300

    live messaging rooms implemented

--- client/screens/Chat.jsx
+++ client/screens/Chat.jsx
@@ -2,7 +2,18 @@ import React, { useState, useLayoutEffect, useCallback } from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
 import { auth, fireStoreDb } from '../config/firebase';
-import { collection, addDoc, orderBy, query, onSnapshot } from 'firebase/firestore';
+import {
+    collection,
+    query,
+    where,
+    orderBy,
+    onSnapshot,
+    addDoc,
+    serverTimestamp,
+    doc,
+    setDoc,
+    getDocs,
+} from 'firebase/firestore';
 
 import { useUser } from '../contexts/UserContext';
 
@@ -12,13 +23,40 @@ const avatarLocalImg = require('../assets/avatar.png');
 
 const Chat = ({ navigation }) => {
     const [messages, setMessages] = useState([]);
-
     const { handleSignOut } = useUser();
+    const otherUserId = 'tycoJeTqx2gdJJoyj1MvoE2pFpj1';
+    const otherUserId2 = 'au2B1vguBTOA2zZQp6VVcGoMt1C2';
+    const currentUser = auth.currentUser.uid;
 
-    // const handleLogout = async () => {
-    //     await auth.signOut();
-    //     navigation.navigate('SignIn');
-    // };
+    const getOrCreateChat = async () => {
+        const participants = [currentUser];
+        if (otherUserId !== currentUser) {
+            participants.push(otherUserId);
+        } else if (otherUserId2) {
+            participants.push(otherUserId2);
+        }
+
+        participants.sort();
+
+        const chatRef = collection(fireStoreDb, 'chats');
+
+        console.log('currentuser chat', currentUser);
+
+        const q = query(chatRef, where('participant_ids', '==', participants));
+
+        const querySnapshot = await getDocs(q);
+
+        if (querySnapshot.empty) {
+            const newChatRef = doc(collection(fireStoreDb, 'chats'));
+            await setDoc(newChatRef, {
+                participant_ids: participants,
+                createdAt: serverTimestamp(),
+            });
+            return newChatRef;
+        } else {
+            return querySnapshot.docs[0].ref;
+        }
+    };
 
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -28,34 +66,74 @@ const Chat = ({ navigation }) => {
                 </TouchableOpacity>
             ),
         });
-        
-        const collectionRef = collection(fireStoreDb, 'chats');
-        const q = query(collectionRef, orderBy('createdAt', 'desc'));
 
-        const unsubscribe = onSnapshot(q, (snapshot) => {
-            setMessages(
-                snapshot.docs.map((doc) => ({
-                    _id: doc.id,
-                    text: doc.data().text,
-                    createdAt: doc.data().createdAt.toDate(),
-                    user: doc.data().user,
-                }))
-            );
-        });
+        const setupMessagesListener = async () => {
+            const chatRef = await getOrCreateChat();
+            const messagesRef = collection(chatRef, 'messages');
+            const q = query(messagesRef, orderBy('createdAt', 'desc'));
 
-        return unsubscribe;
+            const unsubscribe = onSnapshot(q, (snapshot) => {
+                setMessages(
+                    snapshot.docs.map((doc) => ({
+                        _id: doc.id,
+                        text: doc.data().text,
+                        createdAt: doc.data().createdAt.toDate(),
+                        user: doc.data().user,
+                    }))
+                );
+            });
+
+            return unsubscribe;
+        };
+
+        // const collectionRef = collection(fireStoreDb, 'chats');
+        // const q = query(collectionRef, orderBy('createdAt', 'desc'));
+
+        // const unsubscribe = onSnapshot(q, (snapshot) => {
+        //     setMessages(
+        //         snapshot.docs.map((doc) => ({
+        //             _id: doc.id,
+        //             text: doc.data().text,
+        //             createdAt: doc.data().createdAt.toDate(),
+        //             user: doc.data().user,
+        //         }))
+        //     );
+        // });
+
+        // return unsubscribe;
+
+        const unsubscribe = setupMessagesListener();
+        return () => unsubscribe.then((unsub) => unsub());
     }, []);
 
-    const onSend = useCallback((messages = []) => {
-        setMessages((previousMessages) => GiftedChat.append(previousMessages, messages));
+    const onSend = useCallback(async (messages = []) => {
+        // setMessages((previousMessages) => GiftedChat.append(previousMessages, messages));
 
-        const { _id, text, createdAt, user } = messages[0];
-        addDoc(collection(fireStoreDb, 'chats'), {
-            _id,
-            text,
-            createdAt,
-            user,
+        // const { _id, text, createdAt, user } = messages[0];
+        // addDoc(collection(fireStoreDb, 'chats'), {
+        //     _id,
+        //     text,
+        //     createdAt,
+        //     user,
+        // });
+
+        const chatRef = await getOrCreateChat();
+
+        if (!chatRef) return;
+
+        const messagesRef = collection(chatRef, 'messages');
+
+        messages.forEach(async (message) => {
+            const { _id, text, createdAt, user } = message;
+            await addDoc(messagesRef, {
+                _id,
+                text,
+                createdAt,
+                user,
+            });
         });
+
+        setMessages((previousMessages) => GiftedChat.append(previousMessages, messages));
     });
 
     return (
