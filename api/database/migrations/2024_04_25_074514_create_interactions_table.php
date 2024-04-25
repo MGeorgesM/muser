94fb0102cca3dedb7e673a09e6f0a172e5010d7c
George <g.m.6@live.com>
2024-04-25 11:38:27 +0300

    api interactions  migration fix

--- api/database/migrations/2024_04_25_074514_create_interactions_table.php
+++ api/database/migrations/2024_04_25_074514_create_interactions_table.php
@@ -13,10 +13,8 @@ return new class extends Migration
     {
         Schema::create('venues_ratings', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('venue_id');
-            $table->foreign('venue_id')->references('id')->on('venues')->onDelete('cascade')->onUpdate('cascade');
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('venue_id')->references('id')->on('venues')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('interactions');
+        Schema::dropIfExists('venues_ratings');
     }
 };
