462135bf32f960219c31e53f878de756f8d4aef0
George <g.m.6@live.com>
2024-04-27 19:37:38 +0300

    initializing redux slices/store

--- client/store/store.js
+++ client/store/store.js
@@ -1,2 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import venueS
+import usersReducer, {usersSliceName} from "./Users";
+import showsReducer, {showsSliceName} from "./Shows";
+import venuesReducer, {venuesSliceName} from "./Venues";
+
+
+export const store = configureStore({
+    reducer: {
+        [usersSliceName]: usersReducer,
+        [showsSliceName]: showsReducer,
+        [venuesSliceName]: venuesReducer,
+    },
+})
