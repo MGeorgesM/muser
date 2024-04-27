89a2e695d75d6f4284a43053427e396a9247478d
George <g.m.6@live.com>
2024-04-27 12:14:05 +0300

    client/api linking userContext refactoring

--- client/screens/Authentication.jsx
+++ client/screens/Authentication.jsx
@@ -5,15 +5,16 @@ import { Image, View, Text, TextInput, StyleSheet, ScrollView, TouchableOpacity 
 import SignInForm from '../components/AuthenticationForms/SignInForm';
 import SignUpForm from '../components/AuthenticationForms/SignUpForm';
 
+const { styles } = require('../components/AuthenticationForms/styles');
 const logoImg = require('../assets/logo.png');
 
 const Authentication = ({ navigation }) => {
     const [switchHandler, setSwitchHandler] = useState(false);
+    const [error, setError] = useState(null);
     const [signInForm, setSignInForm] = useState({
         email: '',
         password: '',
     });
-    const [error, setError] = useState(null);
 
     const { handleSignIn, handleSignUp } = useUser();
 
@@ -21,86 +22,22 @@ const Authentication = ({ navigation }) => {
         <ScrollView contentContainerStyle={styles.scrollContainer}>
             <View style={styles.topInnerContainer}>
                 <Image style={styles.welcomeLogo} source={logoImg} />
-                <Text style={styles.header}>{switchHandler ? 'Welcome Back!' : 'Join Muser'}</Text>
-                {switchHandler ? <SignInForm signInForm={signInForm} setSignInForm={setSignInForm} /> : <SignUpForm />}
+                <Text style={styles.header}>{switchHandler ? 'Join Muser' : 'Welcome Back!'}</Text>
+                {switchHandler ? <SignUpForm /> : <SignInForm signInForm={signInForm} setSignInForm={setSignInForm} />}
             </View>
             <View style={styles.bottomInnerContainer}>
                 <TouchableOpacity style={styles.primaryBtn} onPress={() => handleSignIn(email, password)}>
                     <Text style={styles.primaryBtnText}>Log In</Text>
                 </TouchableOpacity>
                 <Text style={styles.promptText}>
-                    Don't have an account?{' '}
+                    {switchHandler ? 'Have an account?' : "Don't have an account? "}
                     <Text style={styles.promptLink} onPress={() => setSwitchHandler(!switchHandler)}>
-                        Register
+                        {switchHandler ? 'Log In' : 'Register'}
                     </Text>
                 </Text>
             </View>
         </ScrollView>
     );
 };
-
-export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    scrollContainer: {
-        flexGrow: 1,
-        justifyContent: 'space-between',
-        paddingHorizontal: 20,
-    },
-    header: {
-        textAlign: 'center',
-        fontSize: 32,
-        fontWeight: 'bold',
-        marginTop: 24,
-        marginBottom: 44,
-    },
-    label: {
-        fontSize: 16,
-        marginBottom: 6,
-        marginLeft: 8,
-    },
-    input: {
-        height: 48,
-        borderRadius: 24,
-        borderWidth: 0.5,
-        borderColor: '#212529',
-        padding: 16,
-        marginBottom: 14,
-    },
-    primaryBtn: {
-        height: 48,
-        borderRadius: 24,
-        backgroundColor: '#212529',
-        borderRadius: 24,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    primaryBtnText: {
-        color: '#fff',
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    promptText: {
-        fontSize: 16,
-        marginTop: 10,
-        textAlign: 'center',
-    },
-    promptLink: {
-        color: 'blue',
-        textDecorationLine: 'underline',
-    },
-    welcomeLogo: {
-        width: 130,
-        height: 130,
-        alignSelf: 'center',
-    },
-    topInnerContainer: {
-        marginTop: 128,
-    },
-    bottomInnerContainer: {
-        marginBottom: 64,
-    },
-});
 
 export default Authentication;
