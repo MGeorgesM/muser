50c8c6c8e4f5c84fb96b1646d5e0f448b697e9f0
George <g.m.6@live.com>
2024-04-27 11:08:28 +0300

    client/api linking

--- client/core/tools/apiRequest.js
+++ client/core/tools/apiRequest.js
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://10.0.2.2:3001/';
+axios.defaults.baseURL = 'http://10.0.2.2:8000/api/';
 
 export const sendRequest = async (method, route, body) => {
     try {
@@ -12,28 +12,24 @@ export const sendRequest = async (method, route, body) => {
             url: route,
             data: body,
             headers: {
-                // Authorization: `Bearer ${token}`,
+                Authorization: `Bearer ${token}`,
                 Accept: 'application/json',
             },
         });
         return response;
     } catch (error) {
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-        } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-        }
-        console.log(error.config);
+        // if (error.response) {
+        //     console.log(error.response.data);
+        //     console.log(error.response.status);
+        //     console.log(error.response.headers);
+        // } else if (error.request) {
+        //     console.log(error.request);
+        // } else {
+        //     console.log('Error', error.message);
+        // }
+        // console.log(error.config);
+
+        console.log('error in request', error);
     }
 };
 
