a0e687428253a0afebd59f901aa79e6626a02213
George <g.m.6@live.com>
2024-04-29 20:19:23 +0300

    client feed refactoring modal

--- client/components/Modals/ModalHigh.jsx
+++ client/components/Modals/ModalHigh.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 import { ChevronLeft } from 'lucide-react-native';
 import { utilities } from '../styles/utilities';
 
-const ModalHigh = ({navigation}) => {
+const ModalHigh = ({ title, navigation, items, renderItem }) => {
     return (
         <View style={styles.main}>
             <View style={[utilities.container, styles.overviewContainer]}>
                 <View style={[utilities.flexRow, utilities.center, { marginBottom: 24 }]}>
-                    <ChevronLeft size={24} color="black" style={{ position: 'absolute', left: 0 }} 
-                    onPress={() => navigation.goBack()}/>
-                    <Text style={[utilities.textL, utilities.textBold]}>Shows</Text>
+                    <ChevronLeft
+                        size={24}
+                        color="black"
+                        style={{ position: 'absolute', left: 0 }}
+                        onPress={() => navigation.goBack()}
+                    />
+                    <Text style={[utilities.textL, utilities.textBold]}>{title}</Text>
                 </View>
 
-                <FlatList
-                    data={shows}
-                    keyExtractor={(item) => item.name}
-                    renderItem={({ item }) => <StreamCard show={item} />}
-                ></FlatList>
+                <FlatList data={items} keyExtractor={(item) => item.name} renderItem={renderItem}></FlatList>
             </View>
         </View>
     );
-}
+};
 
-export default ModalHigh
+export default ModalHigh;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
