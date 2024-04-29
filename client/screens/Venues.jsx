f1a186bc4825373687f5c85c75f139447b73f72d
George <g.m.6@live.com>
2024-04-29 15:14:10 +0300

    client venue screens implementation

--- client/screens/Venues.jsx
+++ client/screens/Venues.jsx
@@ -24,21 +24,25 @@ const Venues = ({ navigation }) => {
         };
     }, []);
 
-    const StreamCard = ({ show }) => {
+    const StreamCard = ({ entity }) => {
+        const { picture, about, name, location, venueType } = entity;
+        const imageUrl = `${profilePicturesUrl + picture}`;
         return (
             <TouchableOpacity style={styles.cardContainer} onPress={navigation.navigate}>
-                <Image source={show.imageUrl} style={styles.backgroundImage} />
+                <Image source={{ uri: imageUrl }} style={styles.backgroundImage} />
                 <View style={styles.overlay}>
                     <View>
-                        <Text style={[styles.streamName]}>{show.name}</Text>
+                        <Text style={[styles.streamName]}>{name}</Text>
 
-                        <Text style={styles.date}>{show.date}</Text>
+                        <Text style={styles.date}>{date || about}</Text>
                     </View>
-                    <View style={styles.avatarsDisplay}>
-                        {show.band.members.map((member) => (
-                            <Image source={member.avatar} style={{ width: 32, height: 32, borderRadius: 16 }} />
-                        ))}
-                    </View>
+                    {date && (
+                        <View style={styles.avatarsDisplay}>
+                            {show.band.members.map((member) => (
+                                <Image source={member.avatar} style={{ width: 32, height: 32, borderRadius: 16 }} />
+                            ))}
+                        </View>
+                    )}
                 </View>
             </TouchableOpacity>
         );
@@ -53,13 +57,13 @@ const Venues = ({ navigation }) => {
                         style={{ position: 'absolute', left: 0 }}
                         onPress={() => navigation.goBack()}
                     />
-                    <Text style={[utilities.textL, utilities.textBold]}>Shows</Text>
+                    <Text style={[utilities.textL, utilities.textBold]}>Venues</Text>
                 </View>
 
                 <FlatList
-                    data={shows}
+                    data={venues}
                     keyExtractor={(item) => item.name}
-                    renderItem={({ item }) => <StreamCard show={item} />}
+                    renderItem={({ item }) => <StreamCard entity={item} />}
                 ></FlatList>
             </View>
         </View>
@@ -68,4 +72,55 @@ const Venues = ({ navigation }) => {
 
 export default Venues;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  main: {
+      flex: 1,
+      backgroundColor: colors.darkGray,
+  },
+  overviewContainer: {
+      marginTop: 64,
+      backgroundColor: 'white',
+      borderTopEndRadius: utilities.borderRadius.xl,
+      borderTopLeftRadius: utilities.borderRadius.xl,
+      paddingTop: 24,
+  },
+
+  cardContainer: {
+      width: '100%',
+      height: 180,
+      overflow: 'hidden',
+      position: 'relative',
+      borderRadius: utilities.borderRadius.m,
+      marginBottom: 16,
+  },
+  backgroundImage: {
+      width: '100%',
+      height: '100%',
+      resizeMode: 'cover',
+  },
+  overlay: {
+      flexDirection: 'row',
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      height: '50%',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      padding: 20,
+      justifyContent: 'space-between',
+  },
+  avatarsDisplay: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      gap: 6,
+  },
+  streamName: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: 'white',
+  },
+  date: {
+      fontSize: 16,
+      color: 'white',
+  },
+});
