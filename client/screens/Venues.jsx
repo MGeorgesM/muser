9ce7324722ebca5aa5374e387124322220788a7c
George <g.m.6@live.com>
2024-04-29 15:20:21 +0300

    client venue screens implementation

--- client/screens/Venues.jsx
+++ client/screens/Venues.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useLayoutEffect } from 'react';
 import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity } from 'react-native';
 
+import { setVenues } from '../store/Venues';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { ChevronLeft } from 'lucide-react-native';
 import { colors, utilities } from '../styles/utilities';
+import { profilePicturesUrl } from '../core/tools/apiRequest';
 import { requestMethods, sendRequest } from '../core/tools/apiRequest';
 
 const Venues = ({ navigation }) => {
@@ -22,13 +24,17 @@ const Venues = ({ navigation }) => {
                 console.log('Error fetching venues:', error);
             }
         };
+        getVenues();
     }, []);
 
-    const StreamCard = ({ entity }) => {
-        const { picture, about, name, location, venueType } = entity;
+    const StreamCard = ({ entity, navigation }) => {
+        const { picture, about, name, location, venueType, date } = entity;
         const imageUrl = `${profilePicturesUrl + picture}`;
         return (
-            <TouchableOpacity style={styles.cardContainer} onPress={navigation.navigate}>
+            <TouchableOpacity
+                style={styles.cardContainer}
+                onPress={() => navigation.navigate('VenueDetails', { entity })}
+            >
                 <Image source={{ uri: imageUrl }} style={styles.backgroundImage} />
                 <View style={styles.overlay}>
                     <View>
@@ -63,7 +69,7 @@ const Venues = ({ navigation }) => {
                 <FlatList
                     data={venues}
                     keyExtractor={(item) => item.name}
-                    renderItem={({ item }) => <StreamCard entity={item} />}
+                    renderItem={({ item }) => <StreamCard entity={item} navigation={navigation} />}
                 ></FlatList>
             </View>
         </View>
@@ -73,54 +79,54 @@ const Venues = ({ navigation }) => {
 export default Venues;
 
 const styles = StyleSheet.create({
-  main: {
-      flex: 1,
-      backgroundColor: colors.darkGray,
-  },
-  overviewContainer: {
-      marginTop: 64,
-      backgroundColor: 'white',
-      borderTopEndRadius: utilities.borderRadius.xl,
-      borderTopLeftRadius: utilities.borderRadius.xl,
-      paddingTop: 24,
-  },
+    main: {
+        flex: 1,
+        backgroundColor: colors.darkGray,
+    },
+    overviewContainer: {
+        marginTop: 64,
+        backgroundColor: 'white',
+        borderTopEndRadius: utilities.borderRadius.xl,
+        borderTopLeftRadius: utilities.borderRadius.xl,
+        paddingTop: 24,
+    },
 
-  cardContainer: {
-      width: '100%',
-      height: 180,
-      overflow: 'hidden',
-      position: 'relative',
-      borderRadius: utilities.borderRadius.m,
-      marginBottom: 16,
-  },
-  backgroundImage: {
-      width: '100%',
-      height: '100%',
-      resizeMode: 'cover',
-  },
-  overlay: {
-      flexDirection: 'row',
-      position: 'absolute',
-      bottom: 0,
-      left: 0,
-      right: 0,
-      height: '50%',
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',
-      padding: 20,
-      justifyContent: 'space-between',
-  },
-  avatarsDisplay: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      gap: 6,
-  },
-  streamName: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      color: 'white',
-  },
-  date: {
-      fontSize: 16,
-      color: 'white',
-  },
+    cardContainer: {
+        width: '100%',
+        height: 180,
+        overflow: 'hidden',
+        position: 'relative',
+        borderRadius: utilities.borderRadius.m,
+        marginBottom: 16,
+    },
+    backgroundImage: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
+    overlay: {
+        flexDirection: 'row',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: '50%',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        padding: 20,
+        justifyContent: 'space-between',
+    },
+    avatarsDisplay: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 6,
+    },
+    streamName: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    date: {
+        fontSize: 16,
+        color: 'white',
+    },
 });
