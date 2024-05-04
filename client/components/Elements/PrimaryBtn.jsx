8e5eb12f5a2842068d219c3331bcc760977de060
George <g.m.6@live.com>
2024-05-04 12:24:14 +0300

    client venue layout fix

---
+++ client/components/Elements/PrimaryBtn.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+import { utilities } from '../../styles/utilities';
+
+const PrimaryBtn = ({ text, handlePress, margin = 0, marginBottom = 64, marginTop = 0 }) => {
+    return (
+        <TouchableOpacity style={[utilities.primaryBtn, { margin, marginBottom, marginTop }]} onPress={handlePress}>
+            <Text style={[utilities.primaryBtnText]}>{text}</Text>
+        </TouchableOpacity>
+    );
+};
+
+export default PrimaryBtn;
+
+const styles = StyleSheet.create({});
--- client/screens/VenueDetails.jsx
+++ client/screens/VenueDetails.jsx
@@ -11,6 +11,7 @@ import { utilities, colors } from '../styles/utilities';
 import { profilePicturesUrl, showsPicturesUrl } from '../core/tools/apiRequest';
 import { sendRequest, requestMethods } from '../core/tools/apiRequest';
 import { formatDateString } from '../core/tools/formatDate';
+import PrimaryBtn from '../components/Elements/PrimaryBtn';
 
 const VenueDetails = ({ route, navigation }) => {
     const { venue } = route.params;
@@ -49,7 +50,7 @@ const VenueDetails = ({ route, navigation }) => {
     }, [venue]);
 
     return (
-        <View style={[styles.container, {backgroundColor:colors.bgDark}]}>
+        <View style={[utilities.flexed, { backgroundColor: colors.bgDark }]}>
             {/* <BackBtn navigation={navigation} /> */}
             <View>
                 <Image
@@ -62,44 +63,44 @@ const VenueDetails = ({ route, navigation }) => {
                 />
 
                 <View style={[utilities.overlay, styles.borderRadiusBottom, { height: 96, gap: 2 }]}>
-                    <Text style={[utilities.textL, utilities.textBold, { color: 'white' }]}>
+                    <Text style={[utilities.textL, utilities.myFontBold, { color: 'white' }]}>
                         {!switchHandler ? venue.name : selectedShow.name}
                     </Text>
-                    <Text style={[utilities.textS, { color: colors.offWhite }]}>
+                    <Text style={[utilities.textS, utilities.myFontRegular, { color: colors.offWhite }]}>
                         {switchHandler ? formatDateString(selectedShow.date) : `${venue.location.name}, Lebanon`}
                     </Text>
                 </View>
             </View>
             <View style={[utilities.container]}>
-                <Text style={[utilities.textM, utilities.textBold, { marginVertical: 18 }]}>
+                <Text style={[utilities.textM, utilities.myFontBold, { marginVertical: 18 }]}>
                     {!switchHandler ? 'Upcoming Shows' : 'Band Members'}
                 </Text>
-
-                <FlatList
-                    data={!switchHandler ? shows : selectedShow.band.members}
-                    keyExtractor={(item) => item.id}
-                    renderItem={({ item }) =>
-                        switchHandler ? (
-                            <BandMemberCard entity={item} navigation={navigation} />
-                        ) : (
-                            <ShowCard
-                                entity={item}
-                                navigation={navigation}
-                                handlePress={() => {
-                                    setSelectedShow(item);
-                                    setSwitchHandler(true);
-                                }}
-                            />
-                        )
-                    }
-                    showsVerticalScrollIndicator={false}
-                />
-                <TouchableOpacity
-                    style={[utilities.primaryBtn, { marginVertical: 20 }]}
-                    onPress={() => navigation.navigate('ShowDetails')}
-                >
-                    <Text style={[utilities.primaryBtnText]}>Book your Show!</Text>
-                </TouchableOpacity>
+                {shows.length > 0 ? (
+                    <FlatList
+                        data={!switchHandler ? shows : selectedShow.band.members}
+                        keyExtractor={(item) => item.id}
+                        renderItem={({ item }) =>
+                            switchHandler ? (
+                                <BandMemberCard entity={item} navigation={navigation} />
+                            ) : (
+                                <ShowCard
+                                    entity={item}
+                                    navigation={navigation}
+                                    handlePress={() => {
+                                        setSelectedShow(item);
+                                        setSwitchHandler(true);
+                                    }}
+                                />
+                            )
+                        }
+                        showsVerticalScrollIndicator={false}
+                    />
+                ) : (
+                    <Text style={[utilities.textM, utilities.myFontRegular, { color: colors.offWhite }]}>
+                        No shows available, check back later!
+                    </Text>
+                )}
+                <PrimaryBtn text={'Book your Show'} />
             </View>
         </View>
     );
@@ -110,15 +111,10 @@ export default VenueDetails;
 const { height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-
     borderRadiusBottom: {
         // borderBottomLeftRadius: 20,
         // borderBottomRightRadius: 20,
     },
-
     entityImage: {
         width: '100%',
         height: height * 0.5,
