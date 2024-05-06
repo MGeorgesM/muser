ecd11c9e3cef459f8eb0e093a83b883fff64321e
George <g.m.6@live.com>
2024-05-06 17:14:09 +0300

    api ai matchmaking initial

--- api/app/Http/Controllers/AiMatchMakingController.php
+++ api/app/Http/Controllers/AiMatchMakingController.php
@@ -6,6 +6,7 @@ use App\Models\Genre;
 use App\Models\Instrument;
 use Illuminate\Http\Request;
 use OpenAI\Laravel\Facades\OpenAI;
+use Illuminate\Support\Facades\Log;
 
 
 class AiMatchMakingController extends Controller
@@ -17,24 +18,29 @@ class AiMatchMakingController extends Controller
         $available_genres_in_db = Genre::all()->pluck('name')->toArray();
         $available_instruments_in_db = Instrument::all()->pluck('name')->toArray();
 
-
         $request->validate([
             'message' => 'required|string'
         ]);
 
-        $result = OpenAI::chat()->create([
-            'model' => 'gpt-3.5-turbo',
-            'responseo_format' => 'json',
-            'messages' => [
-                ['role' => 'system', 'content' => 'You are a music expert, you can help me find the music genre from my message which may include songs or artists'],
-                ['role' => 'system', 'content' => 'You should extract two music genres that goes with my message that may includes artists and or songs.'],
-                ['role' => 'system', 'content' => 'You should suggest the closest genres that goes wiht my input from this array of genres: ' . implode(", ", $available_genres_in_db)],
-                ['role' => 'system', 'content' => 'Always return JSON format no text.'],
+        try {
+            $result = OpenAI::chat()->create([
+                'model' => 'gpt-3.5-turbo',
+                'response_format' => 'json',
+                'messages' => [
+                    ['role' => 'system', 'content' => 'You are a music expert, you can help me find the music genre from my message which may include songs or artists'],
+                    ['role' => 'system', 'content' => 'You should extract two music genres that goes with my message that may includes artists and or songs.'],
+                    ['role' => 'system', 'content' => 'You should suggest the closest genres that goes wiht my input from this array of genres: ' . implode(", ", $available_genres_in_db)],
+                    ['role' => 'system', 'content' => 'Always return JSON format no text.'],
 
-                ['role' => 'user', 'content' => $request->message],              
-            ],
-        ]);
+                    ['role' => 'user', 'content' => $request->message],
+                ],
+            ]);
 
-        echo $result->choices[0]->message->content; // Hello! How can I assist you today?
+            $response = $result->choices[0]->message->content;
+            return response()->json($response);
+        } catch (\Exception $th) {
+            Log::error('Failed to generate response from OpenAI: ' . $e->getMessage());
+            return response()->json(['error' => 'Failed to process your request'], 500);
+        }
     }
 }
