e6e56bb91453fafb0867eb225259e4e9e5123523
George <g.m.6@live.com>
2024-05-06 19:21:51 +0300

    api aimatchmaking setting up ai prompts

--- api/app/Http/Controllers/AiMatchMakingController.php
+++ api/app/Http/Controllers/AiMatchMakingController.php
@@ -17,11 +17,10 @@ class AiMatchMakingController extends Controller
     {
 
 
-        $currect_user_instrument = auth()->user()->instrument->pluck('name')->toArray();
+        $currect_user_instrument_id = auth()->user()->instrument_id;
+        $currect_user_instrument = Instrument::find($currect_user_instrument_id)->name;
 
-        dd($currect_user_instrument);
-
-
+        // dd($currect_user_instrument);
 
         $yourApiKey = getenv('OPENAI_API_KEY');
         $client = OpenAI::client($yourApiKey);
@@ -44,7 +43,8 @@ class AiMatchMakingController extends Controller
                     ['role' => 'system', 'content' => 'You are a music expert, find the music genre from my message that include songs or artists'],
                     ['role' => 'system', 'content' => 'The extracted music genre should be included in this array of genres, if multiple genres applicable choose 2 maximum: ' . implode(", ", $available_genres_in_db)],
                     ['role' => 'system', 'content' => 'My message may include a location, you should extract it and pick 3 locations that are close to it available in this array: ' . implode(", ", $available_locations_in_db)],
-                    ['role' => 'system', 'content' => 'Return JSON format of the music genres found, the closest 3 location.'],
+                    ['role' => 'system', 'content' => 'My message may include an instrument, you should extract it and return the closest instrument found in this array: ' . implode(", ", $available_instruments_in_db)],
+                    ['role' => 'system', 'content' => 'Return JSON format of the music genres found, the closest 3 location. if not found return empty array'],
 
                     ['role' => 'user', 'content' => $request->message],
                 ],
