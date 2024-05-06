ae3129c50f55753c5f7f20ffd7024050091b0ef5
George <g.m.6@live.com>
2024-05-06 21:24:44 +0300

    api routes refactoring

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,16 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\VenueMiddleware;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BandController;
 use App\Http\Controllers\ShowController;
-use App\Http\Controllers\VenueController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\VenueController;
 use App\Http\Controllers\GenreController;
-use App\Http\Middleware\AdminMiddleware;
-use App\Http\Middleware\AuthenticatedMiddleware;
-use App\Http\Middleware\VenueMiddleware;
 use App\Http\Controllers\AiMatchMakingController;
+use App\Http\Middleware\AuthenticatedMiddleware;
 
 Route::group([
 
@@ -25,27 +25,27 @@ Route::group([
     Route::post('logout', [AuthController::class, 'logout']);
 
     Route::group(['prefix' => 'register'], function ($router) {
+
         Route::post('/', [AuthController::class, 'register']);
         Route::post('email', [AuthController::class, 'checkEmail']);
         Route::get('userinfo', [AuthController::class, 'getProperties']);
+
     });
-
-
-    // Route::post('register', [AuthController::class, 'register']);
-    // Route::post('register/email', [AuthController::class, 'checkEmail']);
-    // Route::get('register/userinfo', [AuthController::class, 'getProperties']);
 });
 
 Route::middleware([AuthenticatedMiddleware::class])->group(function () {
 
 
     Route::middleware([AdminMiddleware::class])->group(function () {
+
         Route::delete('users/{id?}', [UserController::class, 'disableUser']);
         Route::delete('bands/{id}', [BandController::class, 'deleteBand']);
         Route::delete('shows/{showId}', [ShowController::class, 'deleteShow']);
+
     });
 
     Route::middleware([VenueMiddleware::class])->group(function () {
+
         Route::put('shows', [ShowController::class, 'updateShow']);
     });
 
@@ -53,6 +53,7 @@ Route::middleware([AuthenticatedMiddleware::class])->group(function () {
 
         Route::get('{venueId}/rating', [VenueController::class, 'getVenueAverageRating']);
         Route::post('{venueId}/rating', [VenueController::class, 'addUpdateRating']);
+        
     });
 
     Route::group(['prefix' => 'users'], function ($router) {
@@ -61,6 +62,7 @@ Route::middleware([AuthenticatedMiddleware::class])->group(function () {
         Route::get('details', [UserController::class, 'getUsersPicturesAndNames']);
         Route::get('type/{role}', [UserController::class, 'getUsersByRole']);
         Route::post('{id}', [UserController::class, 'updateUser']);
+
     });
 
     Route::group(['prefix' => 'bands'], function ($router) {
@@ -68,19 +70,30 @@ Route::middleware([AuthenticatedMiddleware::class])->group(function () {
         Route::get('me', [BandController::class, 'getUserBands']);
         Route::get('{id?}', [BandController::class, 'getBand']);
         Route::post('/', [BandController::class, 'addBand']);
+
     });
 
+    Route::group(['prefix' => 'connections'], function ($router) {
 
-    Route::get('connections', [UserController::class, 'getConnections']);
-    Route::post('connections/{id}', [UserController::class, 'addConnection']);
+        Route::get('/', [UserController::class, 'getConnections']);
+        Route::post('/{id}', [UserController::class, 'addConnection']);
 
+    });
 
+    Route::group(['prefix' => 'shows'], function ($router) {
 
-    Route::get('shows/{showId?}', [ShowController::class, 'getShows']);
-    Route::post('shows', [ShowController::class, 'addShow']);
+        Route::get('shows/{showId?}', [ShowController::class, 'getShows']);
+        Route::post('shows', [ShowController::class, 'addShow']);
+
+    });
+
+    Route::group(['prefix' => 'ai'], function ($router) {
+
+        Route::post('/', [AiMatchMakingController::class, 'getMatch']);
+        Route::post('/match', [AiMatchMakingController::class, 'matchUsers']);
+
+    });
 
     Route::get('genres/', [GenreController::class, 'getGenres']);
 
-    Route::post('aimatch/', [AiMatchMakingController::class, 'getMatch']);
-    Route::post('aimatch/match', [AiMatchMakingController::class, 'matchUsers']);
 });
