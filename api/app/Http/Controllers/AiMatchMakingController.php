7a6731b02f8548d9a4c0c57390a61dfdcb1dda77
George <g.m.6@live.com>
2024-05-06 19:03:11 +0300

    api aimatchmaking setting up controller

--- api/app/Http/Controllers/AiMatchMakingController.php
+++ api/app/Http/Controllers/AiMatchMakingController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Genre;
 use App\Models\Instrument;
 use Illuminate\Http\Request;
-use OpenAI\Laravel\Facades\OpenAI;
+use OpenAI;
 use Illuminate\Support\Facades\Log;
 
 
@@ -14,6 +14,8 @@ class AiMatchMakingController extends Controller
 
     public function getMatch(Request $request)
     {
+        $yourApiKey = getenv('OPENAI_API_KEY');
+        $client = OpenAI::client($yourApiKey);
 
         $available_genres_in_db = Genre::all()->pluck('name')->toArray();
         $available_instruments_in_db = Instrument::all()->pluck('name')->toArray();
@@ -23,13 +25,16 @@ class AiMatchMakingController extends Controller
         ]);
 
         try {
-            $result = OpenAI::chat()->create([
-                'model' => 'gpt-3.5-turbo',
-                'response_format' => 'json',
+            $result = $client->chat()->create([
+                'model' => 'gpt-3.5-turbo-0125',
+                'response_format' => [
+                    'type' => 'json_object'
+                ],        
                 'messages' => [
                     ['role' => 'system', 'content' => 'You are a music expert, find the music genre from my message that include songs or artists'],
                     ['role' => 'system', 'content' => 'The extracted music genres should be included in this array of genres: ' . implode(", ", $available_genres_in_db)],
-                    ['role' => 'system', 'content' => 'Return JSON format of the 2 music genres.'],
+                    ['role' => 'system', 'content' => 'The extracted music genres should be included in this array of genres: ' . implode(", ", $available_genres_in_db)],
+                    ['role' => 'system', 'content' => 'Return JSON format of the two music genres.'],
 
                     ['role' => 'user', 'content' => $request->message],
                 ],
