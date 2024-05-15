7f3f6dc7694a74c0b7025e9b02775b2c15992f3e
George <g.m.6@live.com>
2024-05-15 21:59:47 +0300

    client shows refactoring

--- client/screens/Shows/Shows.jsx
+++ client/screens/Shows/Shows.jsx
@@ -1,51 +1,11 @@
-import React, { useEffect, useState } from 'react';
-
-import { setShows } from '../../store/Shows';
-import { useDispatch, useSelector } from 'react-redux';
-import { useUser } from '../../contexts/UserContext';
-
-import { sendRequest, requestMethods } from '../../core/tools/apiRequest';
-
+import React from 'react';
 import ModalHigh from '../../components/Modals/ModalHigh';
 import ShowVenueCard from '../../components/Cards/ShowVenueCards/ShowVenueCard';
 import LoadingScreen from '../../components/Misc/LoadingScreen/LoadingScreen';
+import useShowsLogic from './showsLogic';
 
 const Shows = ({ navigation }) => {
-    const dispatch = useDispatch();
-    const { currentUser } = useUser();
-    const userIsVenue = currentUser.role.id === 2;
-    const shows = useSelector((global) => global.showsSlice.shows);
-
-    useEffect(() => {
-        getShows();
-    }, [currentUser]);
-
-    const getShows = async () => {
-        try {
-            const response = await sendRequest(
-                requestMethods.GET,
-                `shows?status=set${userIsVenue ? `&venue_id=${currentUser.id}` : ''}`,
-                null
-            );
-            if (response.status !== 200) throw new Error('Failed to fetch shows');
-            dispatch(setShows(response.data));
-        } catch (error) {
-            console.log('Error fetching shows:', error);
-        }
-    };
-
-    const handleCardPress = (show) => {
-        userIsVenue
-            ? navigation.navigate('Venues', {
-                  screen: 'VenueDetails',
-                  params: {
-                      show: show,
-                      switchView: true,
-                  },
-              })
-            : navigation.navigate('StreamView', { show });
-    };
-
+    const { shows, handleCardPress } = useShowsLogic();
     return shows && shows.length > 0 ? (
         <ModalHigh
             title="Upcoming Shows"
---
+++ client/screens/Shows/showsLogic.js
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import { setShows } from '../../store/Shows';
+import { useUser } from '../../contexts/UserContext';
+import { useDispatch, useSelector } from 'react-redux';
+import { sendRequest, requestMethods } from '../../core/tools/apiRequest';
+
+const useShowsLogic = () => {
+    const dispatch = useDispatch();
+    const { currentUser } = useUser();
+    const userIsVenue = currentUser?.role === 'venue';
+    const shows = useSelector((global) => global.showsSlice.shows);
+
+    useEffect(() => {
+        getShows();
+    }, [currentUser]);
+
+    const getShows = async () => {
+        try {
+            const response = await sendRequest(
+                requestMethods.GET,
+                `shows?status=set${userIsVenue ? `&venue_id=${currentUser.id}` : ''}`,
+                null
+            );
+            if (response.status !== 200) throw new Error('Failed to fetch shows');
+            dispatch(setShows(response.data));
+        } catch (error) {
+            console.log('Error fetching shows:', error);
+        }
+    };
+
+    const handleCardPress = (show) => {
+        userIsVenue
+            ? navigation.navigate('Venues', {
+                  screen: 'VenueDetails',
+                  params: {
+                      show: show,
+                      switchView: true,
+                  },
+              })
+            : navigation.navigate('StreamView', { show });
+    };
+    return {
+        shows,
+        handleCardPress,
+    };
+};
+
+export default useShowsLogic;
