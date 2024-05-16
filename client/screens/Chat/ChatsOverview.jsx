dc06afa835ab9d07fbeef7a22360e6c2b6f07495
George <g.m.6@live.com>
2024-05-16 19:46:04 +0300

    client chatoverview refactoring

--- client/screens/Chat/ChatsOverview.jsx
+++ client/screens/Chat/ChatsOverview.jsx
@@ -1,54 +1,13 @@
-import React, { useEffect, useState, useLayoutEffect } from 'react';
+import React from 'react';
 import { View, FlatList } from 'react-native';
-
-import { useUser } from '../../contexts/UserContext';
-
-import { fireStoreDb } from '../../config/firebase';
-import { collection, query, where, onSnapshot } from 'firebase/firestore';
-
 import { colors, utilities } from '../../styles/utilities';
 
 import ChatCard from '../../components/Cards/ChatCard/ChatCard';
 import LoadingScreen from '../../components/Misc/LoadingScreen/LoadingScreen';
+import useChatOverviewLogic from './chatOverviewLogic';
 
 const ChatsOverview = ({ navigation }) => {
-    const [chats, setChats] = useState([]);
-    const { currentUser } = useUser();
-    const [isLoading, setIsLoading] = useState(true);
-
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerTitle: 'Your Connections',
-        });
-    });
-
-    useEffect(() => {
-        if (!currentUser) return;
-        const chatRef = collection(fireStoreDb, 'chats');
-        const q = query(chatRef, where('participantsIds', 'array-contains', currentUser.id));
-
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
-            const chatsArray = [];
-            querySnapshot.forEach((doc) => {
-                chatsArray.push({ id: doc.id, ...doc.data() });
-            });
-
-            console.log('Chat Overview Is Updating')
-
-            chatsArray.sort((a, b) => b.lastMessage.createdAt - a.lastMessage.createdAt);
-            setChats(chatsArray);
-            setIsLoading(false);
-
-            if (!querySnapshot.empty) {
-                return querySnapshot.docs[0].ref;
-            }
-
-            return null;
-        });
-
-        return () => unsubscribe;
-    }, [currentUser]);
-
+    const { chats, isLoading } = useChatOverviewLogic();
     return isLoading ? (
         <View style={[utilities.container, { backgroundColor: colors.bgDark }]}>
             <LoadingScreen />
---
+++ client/screens/Chat/chatOverviewLogic.js
@@ -0,0 +1,50 @@
+import { useEffect, useState, useLayoutEffect } from 'react';
+import { useUser } from '../../contexts/UserContext';
+import { fireStoreDb } from '../../config/firebase';
+import { collection, query, where, onSnapshot } from 'firebase/firestore';
+import { useNavigation } from '@react-navigation/native';
+
+const useChatOverviewLogic = () => {
+    const { currentUser } = useUser();
+    const navigation = useNavigation();
+    const [chats, setChats] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerTitle: 'Your Connections',
+        });
+    });
+
+    useEffect(() => {
+        if (!currentUser) return;
+    
+        const chatRef = collection(fireStoreDb, 'chats');
+        const q = query(chatRef, where('participantsIds', 'array-contains', currentUser.id));
+    
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const chatsArray = [];
+            querySnapshot.forEach((doc) => {
+                chatsArray.push({ id: doc.id, ...doc.data() });
+            });
+            chatsArray.sort((a, b) => {
+                if (a.lastMessage?.createdAt && b.lastMessage?.createdAt) {
+                    return b.lastMessage.createdAt - a.lastMessage.createdAt;
+                }
+                return 0;
+            });
+    
+            setChats(chatsArray);
+            setIsLoading(false);
+        });
+    
+        return () => unsubscribe();
+    }, [currentUser]);
+
+  return {
+    chats,
+    isLoading
+  }
+}
+
+export default useChatOverviewLogic
